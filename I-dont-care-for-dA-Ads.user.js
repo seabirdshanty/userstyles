c83171454c682ee4d6d2cdcee363eb3368614c18
Jamie <seabirdshanty@users.noreply.github.com>
2018-07-22 02:07:14 -0700

    this may be legacy idk

---
+++ I-dont-care-for-dA-Ads.user.js
@@ -0,0 +1,7 @@
+@-moz-document url-prefix("http://deviantart.com/"), url-prefix("http://www.deviantart.com/"),  domain("deviantart.com"), domain("www.deviantart.com")
+{
+  .block-notice
+  {
+    display: none !important;
+  }
+}
