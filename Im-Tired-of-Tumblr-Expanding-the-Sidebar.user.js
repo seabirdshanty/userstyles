94babf70ba460a1a8050292bde19fa27080fe151
Jamie <seabirdshanty@users.noreply.github.com>
2018-07-22 02:08:48 -0700

    Hide the Tumbl sidebar (Works)

---
+++ Im-Tired-of-Tumblr-Expanding-the-Sidebar.user.js
@@ -0,0 +1,27 @@
+@-moz-document url-prefix("http://tumblr.com"), url-prefix("https://tumblr.com"), url-prefix("https://www.tumblr.com"), url-prefix("http://www.tumblr.com")
+{
+/*------------------------------------------------------------------
+
+"I'm Tired of Tumblr Expanding the Sidebar" Anti-Sidebar Code
+by Muffu
+> Use this code in any theme you'd like its the internet its free game
+
+
+------------------------------------------------------------------*/
+
+.l-container.l-container--two-column-dashboard .right_column { 
+    display: none !important; 
+}
+
+.l-container--two-column-dashboard .left_column  {
+    float: none !important;
+    width: 550px !important;
+    margin: 0 auto !important;
+}
+
+.l-container.l-container--two-column-dashboard .left_column .posts { 
+    width: 550px !important;
+    margin: 0 auto !important; 
+} 
+
+}
